122e5e208e2be835de28c0838fcdaa15c56f861e
pupubird <rainchai4240@gmail.com>
2020-02-15 00:52:17 +0800

    added auto open browser

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import tornado.ioloop
 import tornado.web as web
+import webbrowser
 
 public_root = os.path.join(os.path.dirname(__file__), 'public')
 
@@ -23,5 +24,8 @@ settings = dict(
 application = web.Application(handlers, **settings)
 
 if __name__ == "__main__":
+    print("Listening to port 8888")
     application.listen(8888)
+    print("Server starts")
+    webbrowser.open_new_tab("http://localhost:8888/")
     tornado.ioloop.IOLoop.instance().start()
