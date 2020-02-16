ac3325b644e2abcbd0913e7f16e10051189b2970
pupubird <rainchai4240@gmail.com>
2020-02-17 01:42:59 +0800

    Added script to compile all css and js to html

--- compile_css_js_into_html.py
+++ compile_css_js_into_html.py
@@ -1,12 +1,58 @@
 import os
+import re
+ROOT = "./public"
 
-files = os.listdir("./public")
 
-htmls = []
-for html_name in files:
-    if '.html' in html_name:
-        htmls.append(html_name)
+def main():
+    files = os.listdir(ROOT)
 
-for html in htmls:
-    html_name = html.split(".")[0]
-    print(html_name)
+    htmls = []
+    for file_name in files:
+        if '.html' in file_name:
+            htmls.append(file_name)
+    for html in htmls:
+        for file_name in files:
+            insert_into_html(file_name, html)
+        print("________________________")
+
+
+def insert_into_html(file_name, html):
+    name1 = file_name.split(".")[0]
+    name2 = html.split(".")[0]
+
+    if name1 != name2:  # return if its not the same
+        return
+
+    tag, target = "", ""
+
+    if 'css' in file_name:
+        tag = "style"
+        target = "</head>"
+    elif 'js' in file_name:
+        tag = "script"
+        target = "</body>"
+    elif 'html' in file_name:
+        return
+
+    with open(ROOT+"/"+file_name, 'r') as f:
+        output = ""
+        for line in f.readlines():
+            output += line
+        output_template = f"<{tag}>{output}</{tag}>"
+
+        print(file_name, html)
+        with open(ROOT+"/"+html, 'r') as g:
+            html_string = ""
+            for line in g.readlines():
+                html_string += line
+
+            index_head = html_string.index(target)
+            html_string = html_string[:index_head] + \
+                output_template + html_string[index_head:]
+
+            with open(ROOT+"/"+html, 'w') as h:
+                h.writelines(html_string)
+
+
+if __name__ == "__main__":
+    main()
