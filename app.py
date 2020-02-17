82fa833f7ef08d2d100756416a0fe2b5f308789f
pupubird <rainchai4240@gmail.com>
2020-02-17 14:28:15 +0800

    updated requirements.txt

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import compile_external_html
 import git
 
 public_root = os.path.join(os.path.dirname(__file__), './static/')
+ROOT = './public'
+OUTPUT_STATIC = './static'
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -35,7 +37,7 @@ application = web.Application(handlers, **settings)
 if __name__ == "__main__":
 
     # Automatically update the public folder
-    frontend = git.cmd.Git('./public')
+    frontend = git.cmd.Git(ROOT)
     print("Updating website content from ", frontend.remote(verbose=True).split("(fetch)")
           [0].replace("origin\t", ""))
 
@@ -44,9 +46,9 @@ if __name__ == "__main__":
     print("Updated website content.")
 
     # Clean up every file for later compiling
-    files = os.listdir("./static")
+    files = os.listdir(OUTPUT_STATIC)
     for file_name in files:
-        os.remove("./static/"+file_name)
+        os.remove(OUTPUT_STATIC+file_name)
     # compiling into a single html file
     compile_external_html.main()
 
